d7f74810115e0ef0eaf615b515066817e39215ae
Andrew <andymwat@gmail.com>
2020-09-29 17:02:36 -0400

    Added pygame-gui and UI theme file

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
 from pygame import surface
 import pygame
+import pygame_gui
 import libmahjong
 from riichiroyale import MahjongHand, load_image, load_tile
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 1500, 400
 
+
 def _build_icon():
-  front = load_image('resources/tiles/51x68/Chun.png', convert=False)
-  back = load_image('resources/tiles/51x68/Front.png', convert=False)
-  return load_tile(front, back)
+    front = load_image('resources/tiles/51x68/Chun.png', convert=False)
+    back = load_image('resources/tiles/51x68/Front.png', convert=False)
+    return load_tile(front, back)
+
 
 def main():
     # Initialise screen
@@ -24,6 +27,12 @@ def main():
     background = surface.Surface(screen.get_size())
     background = background.convert_alpha()
     background.fill((7, 99, 36))
+
+    # Setup pygame-gui
+    manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT), 'theme.json')
+    newgame_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                                  text='New Game',
+                                                  manager=manager)
 
     # Display some text
     font = pygame.font.Font('build/_deps/sourcesanspro-src/TTF/SourceSans3-Regular.ttf', 36)
@@ -42,23 +51,35 @@ def main():
 
     space_pressed = False
 
+    # Clock for pygame-gui
+    clock = pygame.time.Clock()
+
     # Event loop
     while 1:
+        time_delta = clock.tick(60) / 1000.0
         pygame.event.pump()
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.constants.QUIT:
                 return
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == newgame_button:
+                        print('Clicked new game button.')
+            manager.process_events(event)
         if keys[pygame.constants.K_SPACE] and not space_pressed:
-          space_pressed = True
-          hand.newHand()
+            space_pressed = True
+            hand.newHand()
         if not keys[pygame.constants.K_SPACE]:
-          space_pressed = False
+            space_pressed = False
+        manager.update(time_delta)
         screen.blit(background, (0, 0))
         hand.update()
         hand.draw(screen)
+        manager.draw_ui(screen)
         pygame.display.flip()
         pygame.display.update()
 
+
 if __name__ == "__main__":
-  sys.exit(main())
+    sys.exit(main())
