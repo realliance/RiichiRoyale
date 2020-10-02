60e870664f12722f66ca906bf51d39b56474138c
Andrew <andymwat@gmail.com>
2020-10-02 18:37:02 -0400

    Added switching menus and improved theming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
+import enum
 from math import cos, sin
-
 from pygame import surface
 import pygame
 import pygame_gui
@@ -10,13 +10,127 @@ from riichiroyale import MahjongHand, load_image, load_tile
 SCREEN_WIDTH, SCREEN_HEIGHT = 1500, 800
 
 
+class Menu:
+    uiElements = list()
+    manager = pygame_gui.UIManager
+
+    def processuievent(self, event):
+        print('Got uncaught UI event: ', event)
+
+
+currentMenu = Menu()
+menus = {
+
+}
+
+
 def _build_icon():
     front = load_image('resources/tiles/51x68/Chun.png', convert=False)
     back = load_image('resources/tiles/51x68/Front.png', convert=False)
     return load_tile(front, back)
 
 
+def createmainmenu():
+    menu = Menu()
+    menu.manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT), 'theme.json')
+    newgame_button_rect = pygame.Rect(0, 0, 100, 50)
+    newgame_button_rect.bottomleft = (500, -200)
+    newgame_button = pygame_gui.elements.UIButton(relative_rect=newgame_button_rect,
+                                                  text='New Game',
+                                                  manager=menu.manager,
+                                                  anchors={
+                                                      'top': 'bottom',
+                                                      'bottom': 'bottom',
+                                                      'left': 'left',
+                                                      'right': 'left',
+                                                  })
+
+    loadgame_button_rect = pygame.Rect(0, 0, 100, 50)
+    loadgame_button_rect.bottomleft = (650, -200)
+    loadgame_button = pygame_gui.elements.UIButton(relative_rect=loadgame_button_rect,
+                                                   text='Load Game',
+                                                   manager=menu.manager,
+                                                   anchors={
+                                                       'top': 'bottom',
+                                                       'bottom': 'bottom',
+                                                       'left': 'left',
+                                                       'right': 'left',
+                                                   })
+
+    settings_button_rect = pygame.Rect(0, 0, 100, 50)
+    settings_button_rect.bottomleft = (800, -200)
+    settings_button = pygame_gui.elements.UIButton(relative_rect=settings_button_rect,
+                                                   text='Settings',
+                                                   manager=menu.manager,
+                                                   anchors={
+                                                       'top': 'bottom',
+                                                       'bottom': 'bottom',
+                                                       'left': 'left',
+                                                       'right': 'left',
+                                                   })
+    menu.uiElements.append(newgame_button)
+    menu.uiElements.append(loadgame_button)
+    menu.uiElements.append(settings_button)
+
+    # processuievent() is called when a UI event is caught while this menu is active
+    def processuievent(event):
+        global currentMenu
+        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == newgame_button:
+                print('Pressed new game')
+            if event.ui_element == settings_button:
+                print('Switching to settings menu')
+                currentMenu = menus['settings']
+
+    menu.processuievent = processuievent
+    return menu
+
+
+def createsettingsmenu():
+    menu = Menu()
+    menu.manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT), 'theme.json')
+
+    settingsmenu_rect = pygame.Rect(SCREEN_WIDTH/2 - 350, SCREEN_HEIGHT/2 - 250, 700, 500)
+    settingsmenu_panel = pygame_gui.elements.UIPanel(relative_rect=settingsmenu_rect,
+                                                     starting_layer_height=1,
+                                                     manager=menu.manager,
+                                                     anchors={
+                                                         'top': 'top',
+                                                         'bottom': 'bottom',
+                                                         'left': 'left',
+                                                         'right': 'right'
+                                                     })
+
+    back_button_rect = pygame.Rect(0, 0, 100, 50)
+    back_button_rect.bottomleft = (10, -10)
+    back_button = pygame_gui.elements.UIButton(relative_rect=back_button_rect,
+                                               container=settingsmenu_panel,
+                                               text='Back',
+                                               manager=menu.manager,
+                                               anchors={
+                                                   'top': 'bottom',
+                                                   'bottom': 'bottom',
+                                                   'left': 'left',
+                                                   'right': 'left',
+                                               })
+
+    menu.uiElements.append(settingsmenu_panel)
+    menu.uiElements.append(back_button)
+
+    # processuievent() is called when a UI event is caught while this menu is active
+    def processuievent(event):
+        global currentMenu
+        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == back_button:
+                print('Pressed back')
+                currentMenu = menus['main']
+
+    menu.processuievent = processuievent
+    return menu
+
+
 def main():
+    global currentMenu
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -30,43 +144,11 @@ def main():
     background = background.convert_alpha()
     background.fill((7, 99, 36))
 
-    # Setup pygame-gui
-    manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT), 'theme.json')
-    newgame_button_rect = pygame.Rect(0, 0, 100, 50)
-    newgame_button_rect.bottomleft = (500, -200)
-    newgame_button = pygame_gui.elements.UIButton(relative_rect=newgame_button_rect,
-                                                  text='New Game',
-                                                  manager=manager,
-                                                  anchors={
-                                                      'top': 'bottom',
-                                                      'bottom': 'bottom',
-                                                      'left': 'left',
-                                                      'right': 'left',
-                                                  })
-    loadgame_button_rect = pygame.Rect(0, 0, 100, 50)
-    loadgame_button_rect.bottomleft = (650, -200)
-    loadgame_button = pygame_gui.elements.UIButton(relative_rect=loadgame_button_rect,
-                                                   text='Load Game',
-                                                   manager=manager,
-                                                   anchors={
-                                                       'top': 'bottom',
-                                                       'bottom': 'bottom',
-                                                       'left': 'left',
-                                                       'right': 'left',
-                                                   })
-
-    settings_button_rect = pygame.Rect(0, 0, 100, 50)
-    settings_button_rect.bottomleft = (800, -200)
-    loadgame_button = pygame_gui.elements.UIButton(relative_rect=settings_button_rect,
-                                                   text='Settings',
-                                                   manager=manager,
-                                                   anchors={
-                                                       'top': 'bottom',
-                                                       'bottom': 'bottom',
-                                                       'left': 'left',
-                                                       'right': 'left',
-                                                   })
-
+    # Setup menus
+    mainmenu = createmainmenu()
+    settingsmenu = createsettingsmenu()
+    menus['main'] = mainmenu
+    menus['settings'] = settingsmenu
 
     # Display some text
     font = pygame.font.Font('build/_deps/sourcesanspro-src/TTF/SourceSans3-Regular.ttf', 36)
@@ -77,7 +159,7 @@ def main():
 
     titlefont = pygame.font.Font('build/_deps/sourcesanspro-src/TTF/SourceSans3-BoldIt.ttf', 100)
     titletext = titlefont.render("Riichi Royale", 1, (255, 40, 40))
-    titletextpos = titletext.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    titletextpos = titletext.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
 
     background.blit(text, textpos)
     background.blit(titletext, titletextpos)
@@ -94,6 +176,9 @@ def main():
     # Clock for pygame-gui
     clock = pygame.time.Clock()
 
+    # Menu to draw
+    currentMenu = mainmenu
+
     # Event loop
     while 1:
         time_delta = clock.tick(60) / 1000.0
@@ -102,24 +187,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.constants.QUIT:
                 return
+            # Pass UI events to the current menu for processing
             if event.type == pygame.USEREVENT:
-                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                    if event.ui_element == newgame_button:
-                        print('Clicked new game button.')
-            manager.process_events(event)
+                currentMenu.processuievent(event)
+            # Pass window events to pygame-gui for processing
+            currentMenu.manager.process_events(event)
         if keys[pygame.constants.K_SPACE] and not space_pressed:
             space_pressed = True
             hand.newHand()
         if not keys[pygame.constants.K_SPACE]:
             space_pressed = False
-        manager.update(time_delta)
+        # Update the state of the current menu
+        currentMenu.manager.update(time_delta)
         screen.blit(background, (0, 0))
         hand.update()
         for index, sprite in enumerate(hand.sprites()):
             sprite.rect = ((SCREEN_WIDTH / 2 - 100) * cos(index + pygame.time.get_ticks() / 5000.0) + 700,
                            (SCREEN_HEIGHT / 2 - 100) * sin(index + pygame.time.get_ticks() / 5000.0) + 350)
         hand.draw(screen)
-        manager.draw_ui(screen)
+        currentMenu.manager.draw_ui(screen)
         pygame.display.flip()
         pygame.display.update()
 
