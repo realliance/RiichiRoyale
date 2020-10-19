03d9feacc90068fed3d38ddfa2767ffef14465a4
Christopher Miller <admin@mctherealm.net>
2020-10-19 13:38:32 -0400

    Attempted to export all symbols

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="riichiroyale",
-    version="0.1.15",
+    version="0.1.16",
     author="Christopher Miller",
     author_email="cmiller548@gmail.com",
     description="Singleplayer Riichi Mahjong Game",
