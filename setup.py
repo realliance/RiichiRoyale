6c90183e3a2ba92a54a912620492af0830602ab3
Christopher Miller <admin@mctherealm.net>
2020-10-19 11:53:45 -0400

    Attempted enforcement of python 3.8 when building

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="riichiroyale",
-    version="0.1.11",
+    version="0.1.12",
     author="Christopher Miller",
     author_email="cmiller548@gmail.com",
     description="Singleplayer Riichi Mahjong Game",
