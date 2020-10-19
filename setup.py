bf77d0d7c0df67b84e58da1158ff74793c8f15bd
Christopher Miller <admin@mctherealm.net>
2020-10-19 12:55:01 -0400

    Attempted deps addition

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="riichiroyale",
-    version="0.1.14",
+    version="0.1.15",
     author="Christopher Miller",
     author_email="cmiller548@gmail.com",
     description="Singleplayer Riichi Mahjong Game",
@@ -13,7 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     install_requires=[
         'pygame==2.0.0.dev14',
-        'pygame-gui==0.5.7'
+        'pygame-gui==0.5.7',
+        'pybind11~=2.5'
     ],
     url="https://github.com/HartleyAHartley/RiichiRoyale",
     packages=['libmahjong', 'riichiroyale', 'riichiroyale.resources', 'riichiroyale.resources.fonts', 'riichiroyale.audio', 'riichiroyale.sprites', 'riichiroyale.sprites.elements', 'riichiroyale.utils', 'riichiroyale.test', 'riichiroyale.game', 'riichiroyale.tutorial', 'riichiroyale.views'],
