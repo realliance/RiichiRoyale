5c85f1c1352d186658ff27872ead01db4c8f0dd4
Connor Bolick <cbolick314@gmail.com>
2020-10-19 09:28:17 -0400

    Adding comments
    added some comments to soundmanager.py

--- riichiroyale/audio/soundmanager.py
+++ riichiroyale/audio/soundmanager.py
@@ -9,21 +9,24 @@ class SoundManager():
 		self.music_playlist = []
 		self.sfx_sets = dict()
 		self.current_playlist_song = 0
-		self.master_volume = 1
-		self.music_volume = 1
-		self.sfx_volume = 1
-
+		self.master_volume = 1 #acceptable values 0 to 1
+		self.music_volume = 1 #percernatage of master volume(acceptable values 0 to 1)
+		self.sfx_volume = 1 #percernatage of master volume(acceptable values 0 to 1)
+	
+	#define new sound set
 	def register_sfx_set(self, name):
 		self.sfx_sets[name] = []
 
+	# add to sound set
 	def add_to_sfx_set(self, name, sfx):
 		self.sfx_sets[name] += [sfx]
 
+	#play from set
 	def play_from_set(self, set_name):
 		self.play_sfx(random.choice(self.sfx_sets[set_name]))
 
 	def add_audio_source(self, name, path, preload=True):
-		if preload:
+		if preload: #default is to preload
 			sound_obj = Sound(path)
 		else:
 			sound_obj = None
@@ -33,14 +36,14 @@ class SoundManager():
 		if name not in self.audio_sources:
 			raise "Attempted to play unregistered audio source"
 		preloaded, path, sound_obj = self.audio_sources[name]
-		if not preloaded and preload_if_not:
+		if not preloaded and preload_if_not: #preload logic
 			sound_obj = Sound(path)
 			self.audio_sources[name] = [True, path, sound_obj]
-		sound_obj.set_volume(self.master_volume * self.sfx_volume)
+		sound_obj.set_volume(self.master_volume * self.sfx_volume) #set volume before playing
 		sound_obj.play()
 
 	def on_update(self):
-		if not pygame.mixer.music.get_busy():
+		if not pygame.mixer.music.get_busy(): #plays next song when previous song ends
 			self.current_playlist_song += 1
 			if self.current_playlist_song >= len(self.current_playlist_song):
 				self.current_playlist_song = 0
@@ -54,7 +57,7 @@ class SoundManager():
 		pygame.mixer.music.play()
 
 	def play_music(self, name):
-		if name not in self.audio_sources:
+		if name not in self.audio_sources: #exception handling
 			raise "Attempted to play unregistered audio source"
 		path = self.audio_sources[name][1]
 		pygame.mixer.music.load(path)
@@ -63,7 +66,7 @@ class SoundManager():
 
 	def stop_music(self):
 		pygame.mixer.music.stop()
-		pygame.mixer.music.rewind()
+		pygame.mixer.music.rewind() #restarts sing so that if play is called it is at the beginning
 
 	def set_music_volume(self, volume):
 		self.music_volume = volume
