e3dbc64315f2ff526dfae58c78b062336ac831bd
Christopher Miller <admin@mctherealm.net>
2020-11-29 13:22:21 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="riichiroyale",
-    version="1.0.2",
+    version="1.0.3",
     author="Christopher Miller",
     author_email="cmiller548@gmail.com",
     description="Singleplayer Riichi Mahjong Game",
