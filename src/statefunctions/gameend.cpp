2d92180a16c7469c8cd0b1ebe1d4573ec5630890
Alice Hartley <alicehartleyhartley@gmail.com>
2020-11-29 20:47:52 -0500

    Python Managed controllers no longer cleaned up by c++

--- src/statefunctions/gameend.cpp
+++ src/statefunctions/gameend.cpp
@@ -6,15 +6,21 @@
 #include "player.h"
 #include "playercontroller.h"
 #include "mahjongns.h"
+#include "controllermanager.h"
 using namespace Mahjong;
 
 auto Mahjong::GameEnd(GameState& state) -> GameState& {
   std::cout << "Scores: ";
   for(auto & player : state.players){
     player.controller->ReceiveEvent(END_EVENT);
+    std::cout << player.points << ", ";
+    #ifndef NO_PYBIND
+      if(pythonManagedControllers.contains(player.controller->Name())){
+        continue;
+      }
+    #endif
     delete player.controller;
     player.controller = nullptr;
-    std::cout << player.points << ", ";
   }
   std::cout << std::endl;
   return state;
